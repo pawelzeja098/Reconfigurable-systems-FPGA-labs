1c334ef8f5b5e6e378efe8a661ad7685c60c0779
Pawel Zeja <130493752+pawelzeja098@users.noreply.github.com>
2025-05-21 14:48:05 +0200

    Update vision_system.v
    add centroid vis module

--- labs_binaryzation_on_fpga/vision_system.v
+++ labs_binaryzation_on_fpga/vision_system.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 05.04.2025 15:23:40
+// Create Date: 21.05.2025 12:47:25
 // Design Name: 
-// Module Name: vision_system
+// Module Name: vis_centroid
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,120 +20,51 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module vision_system(
+module vis_centroid
+#(
+    parameter IMG_H = 64,
+    parameter IMG_W = 64
+)(
     input clk,
-    input de_in,
-    input hsync_in,
-    input vsync_in,
-    input [23:0] pixel_in,
-    input [3:0] sw,
+    input de,
+    input hsync,
+    input vsync,
+    input[23:0] pixel_in,
+    input[10:0] x,
+    input[10:0] y,   
     
     output de_out,
     output hsync_out,
     output vsync_out,
-    output [23:0] pixel_out
+    output[23:0] pixel_out
     );
-    
-//MULTIPLEXER
-wire [23:0]rgb_mux[15:0];
-wire de_mux[15:0];
-wire hsync_mux[15:0];
-wire vsync_mux[15:0];
 
+reg [10:0] x_pos = 11'd0;
+reg [10:0] y_pos = 11'd0;
 
-assign rgb_mux[0] = pixel_in;
+always @(posedge clk)
+begin
+    if(vsync == 1'b1) 
+    begin
+        x_pos <= 11'd0;
+        y_pos <= 11'd0;
+    end
+    else
+    begin
+        if(de == 1'b1)
+        begin
+            x_pos <= x_pos + 1;
+            if(x_pos == IMG_W - 1)
+            begin
+                x_pos <= 11'b0;
+                y_pos <= y_pos + 1;
+            end
+         end
+    end 
+end
 
-assign de_mux[0] = de_in;
-assign hsync_mux[0] = hsync_in;
-assign vsync_mux[0] = vsync_in;
+assign {de_out,vsync_out,hsync_out} = {de,vsync,hsync};
 
-
-
-//assign de_out = de_in;
-//assign hsync_out = hsync_in;
-//assign vsync_out = vsync_in;
-//assign pixel_out = pixel_in;
-
-//wire [7:0] r_in = pixel_in[23:16];
-//wire [7:0] g_in = pixel_in[15:8];
-//wire [7:0] b_in = pixel_in[7:0];
-
-//wire [7:0] r_out;
-//wire [7:0] g_out;
-//wire [7:0] b_out;
-
-//LUT lut_r (
-//    .clk(clk),
-//    .a   (r_in),
-//    .qspo (r_out)
-//);
-
-//LUT lut_g (
-//    .clk(clk),
-//    .a   (g_in),
-//    .qspo (g_out)
-//);
-
-//LUT lut_b (
-//    .clk(clk),
-//    .a   (b_in),
-//    .qspo (b_out)
-//);
-
-
-
-rgb2ycbcr_3 rgb2ycbcr(
-    .clk(clk),
-    .de_in(de_mux[0]),
-    .hsync_in(hsync_mux[0]),
-    .vsync_in(vsync_mux[0]),
-    .pixel_in(rgb_mux[0]),
-    
-    .de_out(de_mux[1]),
-    .hsync_out(hsync_mux[1]),
-    .vsync_out(vsync_mux[1]),
-    .pixel_out(rgb_mux[1])
-
-
-);
-
-assign pixel_out = rgb_mux[sw];
-assign de_out = de_mux[sw];
-assign hsync_out = hsync_mux[sw];
-assign vsync_out = vsync_mux[sw];
-
-
-//assign rgb_mux[1] = pixel_out;
-
-//assign de_mux[1] = de_out;
-//assign hsync_mux[1] = hsync_out;
-//assign vsync_mux[1] = vsync_out;
-
-
-
-
-// wire and_result;
-
-
-//wire [7:0] bin_value;
-//assign bin_value = (r_out && g_out && b_out) ? 8'd255 : 8'd0;
-////assign bin_value = (r_out && g_out && b_out);
-//assign pixel_out = {bin_value, bin_value, bin_value}; // RGB = 255 lub 0
-
-//assign pixel_out = {r_out, g_out, b_out};
-//reg r_de = 0;
-//reg r_hsync = 0;
-//reg r_vsync = 0;
-//always @(posedge clk)
-//begin
-//    r_de <= de_in;
-//    r_hsync <= hsync_in;
-//    r_vsync <= vsync_in;
-//end
-
-//assign de_out = r_de;
-//assign hsync_out = r_hsync;
-//assign vsync_out = r_vsync;
-
-
+assign pixel_out[23:16] = (x_pos == x || y_pos == y) ? 8'd255 : pixel_in[23:16];
+assign pixel_out[15:0] = (x_pos == x || y_pos == y) ? 16'd0 : pixel_in[15:0];
 endmodule
